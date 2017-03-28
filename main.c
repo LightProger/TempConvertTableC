7d1a4b8c06cb3df3586a85da9862c9722692fa29
Alexandr <photoluxe@mail.ru>
2017-03-29 00:53:47 +0600

    Print table fahr-celsius

--- main.c
+++ main.c
@@ -5,17 +5,17 @@
 
 int main() {
 
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0; // Нижняя граница температур
     upper = 300; // Верхняя граница температур
-    step = 2; // Шаг
+    step = 1; // Шаг
 
     fahr = lower;
     while(fahr <= upper){
         celsius = 5 * (fahr - 32) / 9;
-        printf ("%d\t%d\n", fahr, celsius);
+        printf ("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
